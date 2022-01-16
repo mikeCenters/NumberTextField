5fd2f1daa91dcbafbdbf9d6a709cd8ffa76c82b4
Mike Centers <michael.centers@icloud.com>
2022-01-16 03:40:46 -0500

    Implemented currency bounds for cursor placement.
    This method will limit cursor movement across the currency symbol.

--- Sources/NumberTextField/Components/NumberCoordinator+Cursor.swift
+++ Sources/NumberTextField/Components/NumberCoordinator+Cursor.swift
@@ -14,9 +14,9 @@ extension NumberTextFieldViewRep.Coordinator {
      Move the cursor of the `TextField` within predetermined boundaries.
      
      The formatted strings will sometimes have symbols that show what the value of the `String` represent
-     (currency, percentage, etc.). This method is used to prevent the cursor from crossing these symbols;
-     therefore, allowing the user to input at inappropriate locations. This method requires the
-     `NumberFormatter` of the `ViewRepresentable` to determine how to manipulate the cursor.
+     (currency, percentage, etc.). This method is used to prevent the cursor from crossing these symbols.
+     This method requires the `NumberFormatter` of the `ViewRepresentable` to determine how to
+     manipulate the cursor.
      
      - parameter textField: The textfield that will have it's cursor manipulated.
      - parameter cursorPosition: The index of the cursor's current position.
@@ -26,6 +26,9 @@ extension NumberTextFieldViewRep.Coordinator {
         switch self.viewRep.formatter.numberStyle {
         case .percent:
             self._withinPercentBounds(textField, cursorPosition: cursorPosition)
+            
+        case .currency:
+            self._withinCurrencyBounds(textField, cursorPosition: cursorPosition)
             
         default:
             return
@@ -42,11 +45,40 @@ extension NumberTextFieldViewRep.Coordinator {
      */
     private func _withinPercentBounds(_ textField: UITextField, cursorPosition: Int) {
         guard cursorPosition == textField.text?.count,
-              let newPosition = textField.position(from: textField.endOfDocument, offset: -1)
+              let endingOffset = textField.position(from: textField.endOfDocument, offset: -1)
         else {
             return
         }
         
-        textField.selectedTextRange = textField.textRange(from: newPosition, to: newPosition)
+        textField.selectedTextRange = textField.textRange(from: endingOffset, to: endingOffset)
+    }
+    
+    /**
+     Move the cursor within the boundaries of a currency number style.
+     
+     The cursor will not be allowed to cross the currency character position.
+     
+     - parameter textField: The textfield that will have it's cursor manipulated.
+     - parameter cursorPosition: The index of the cursor's current position.
+     */
+    private func _withinCurrencyBounds(_ textField: UITextField, cursorPosition: Int) {
+        guard let text = textField.text,
+              let beginningOffset = textField.position(from: textField.beginningOfDocument, offset: 1),
+              let endingOffset = textField.position(from: textField.endOfDocument, offset: -1)
+        else {
+            return
+        }
+        
+        if text.first == self.currencyChar {
+            if cursorPosition == 0 {
+                textField.selectedTextRange = textField.textRange(from: beginningOffset, to: beginningOffset)
+            }
+        }
+        
+        if text.last == self.currencyChar {
+            if cursorPosition == text.count {
+                textField.selectedTextRange = textField.textRange(from: endingOffset, to: endingOffset)
+            }
+        }
     }
 }
