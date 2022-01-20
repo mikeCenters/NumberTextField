afa713e38587f311781423201abf6e5874939d96
Mike Centers <michael.centers@icloud.com>
2022-01-19 23:42:46 -0500

    Set the hugging priority for the textField to resolve View rendering fullscreen.
    Implemented a setModifiers method to apply view modifiers.
    Supported:
    	- Font
    	- TextAlignment
    
    Added the decimal pad as the keyboard type.

--- Sources/NumberTextField/Components/NumberTextFieldViewRep.swift
+++ Sources/NumberTextField/Components/NumberTextFieldViewRep.swift
@@ -9,6 +9,9 @@ import SwiftUI
 import UIKit
 
 struct NumberTextFieldViewRep: UIViewRepresentable {
+    @Environment(\.multilineTextAlignment) private var textAlignment: TextAlignment
+    @Environment(\.font) private var font: Font?
+    
     var placeholder: String
     @Binding var value: Decimal?
     var formatter: NumberFormatter
@@ -17,23 +20,97 @@ struct NumberTextFieldViewRep: UIViewRepresentable {
     
     
     func makeUIView(context: UIViewRepresentableContext<NumberTextFieldViewRep>) -> UITextField {
-        let textField = UIOpenTextField()
+        let textField = UIOpenTextField(frame: .zero)
         textField.delegate = context.coordinator
         textField.placeholder = self.placeholder
-        
-        textField.autocorrectionType = .no
         context.coordinator.setup(textField)
         
+        textField.setContentHuggingPriority(.required, for: .horizontal)
+        textField.setContentHuggingPriority(.required, for: .vertical)
+        
+        self.setModifiers(textField, environment: context.environment)
         return textField
     }
     
-    func updateUIView(_ uiView: UITextField, context: UIViewRepresentableContext<NumberTextFieldViewRep>) {
+    func updateUIView(_ textField: UITextField, context: UIViewRepresentableContext<NumberTextFieldViewRep>) {
+        self.setModifiers(textField, environment: context.environment)
+        
         DispatchQueue.main.async {
-            context.coordinator.updateText(uiView)
+            context.coordinator.updateText(textField)
         }
     }
     
     func makeCoordinator() -> NumberTextFieldViewRep.Coordinator {
         Coordinator(self)
+    }
+}
+
+
+// MARK: - Modifiers
+extension NumberTextFieldViewRep {
+    /**
+     Set the view modifiers for the `UITextField`
+     
+     This method applies the modifiers from the environment to the text field.
+     
+     - parameter textField: The `UITextField` that will be modified.
+     - parameter environment: The `EnvironmentValues` set for the `View` hierarchy.
+     */
+    private func setModifiers(_ textField: UITextField, environment: EnvironmentValues) {
+        textField.keyboardType = .decimalPad
+        
+        textField.textAlignment = NSTextAlignment.getAlignment(from: self.textAlignment)
+        
+        if let font = self.font {
+            textField.font = UIFont.preferredFont(from: font)
+        }
+    }
+}
+
+
+// MARK: - NSTextAlignment
+extension NSTextAlignment {
+    static func getAlignment(from alignment: TextAlignment) -> NSTextAlignment {
+        switch alignment {
+        case .leading:
+            return .left
+        case .center:
+            return .center
+        case .trailing:
+            return .right
+        }
+    }
+}
+
+
+// MARK: - UIFont
+extension UIFont {
+    class func preferredFont(from font: Font) -> UIFont {
+        switch font {
+        case .largeTitle:
+            return UIFont.preferredFont(forTextStyle: .largeTitle)
+        case .title:
+            return UIFont.preferredFont(forTextStyle: .title1)
+        case .title2:
+            return UIFont.preferredFont(forTextStyle: .title2)
+        case .title3:
+            return UIFont.preferredFont(forTextStyle: .title3)
+        case .headline:
+            return UIFont.preferredFont(forTextStyle: .headline)
+        case .subheadline:
+            return UIFont.preferredFont(forTextStyle: .subheadline)
+        case .callout:
+            return UIFont.preferredFont(forTextStyle: .callout)
+        case .caption:
+            return UIFont.preferredFont(forTextStyle: .caption1)
+        case .caption2:
+            return UIFont.preferredFont(forTextStyle: .caption2)
+        case .footnote:
+            return UIFont.preferredFont(forTextStyle: .footnote)
+        case .body:
+            return UIFont.preferredFont(forTextStyle: .body)
+        default:
+            return UIFont.preferredFont(forTextStyle: .body)
+        }
     }
 }
