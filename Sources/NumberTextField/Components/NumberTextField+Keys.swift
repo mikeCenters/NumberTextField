689ec3ab9f72cc45c120e8af62a13dbe17212d23
Mike Centers <michael.centers@icloud.com>
2022-01-21 22:06:31 -0500

    Implemented EnvironmentKeys for customization of NumberTextField.
    - Text Color
    - Text Alignment
    - Font

---
+++ Sources/NumberTextField/Components/NumberTextField+Keys.swift
@@ -0,0 +1,114 @@
+//
+//  NumberTextField+Keys.swift
+//  
+//
+//  Created by Michael Centers on 1/20/22.
+//
+
+import SwiftUI
+
+// MARK: - Keys
+extension View {
+    ///. Sets the environment value of `NumberTextField_TextColor`.
+    @inlinable public func textColor(_ color: Color) -> some View {
+        environment(\.numberTextField_TextColor, color)
+    }
+    
+    /// Sets the environment value of `NumberTextField_TextAlignment`.
+    @inlinable public func textAlignment(_ alignment: NSTextAlignment) -> some View {
+        environment(\.numberTextField_TextAlignment, alignment)
+    }
+    
+    /// Sets the environment value of `NumberTextField_Font`.
+    @inlinable public func uiFont(_ font: Font, weight: UIFont.Weight = .regular) -> some View {
+        environment(\.numberTextField_Font, UIFont.preferredFont(from: font).withWeight(weight))
+    }
+}
+
+
+// MARK: - Text Color
+extension EnvironmentValues {
+    ///. The foreground color of text in a `NumberTextField`.
+    public var numberTextField_TextColor: Color {
+        get { self[NumberTextField_TextColor.self] }
+        set { self[NumberTextField_TextColor.self] = newValue }
+    }
+}
+
+private struct NumberTextField_TextColor: EnvironmentKey {
+    static let defaultValue: Color = .primary
+}
+
+
+// MARK: - Text Alignment
+extension EnvironmentValues {
+    ///. The text alignment in a `NumberTextField`.
+    public var numberTextField_TextAlignment: NSTextAlignment {
+        get { self[NumberTextField_TextAlignment.self] }
+        set { self[NumberTextField_TextAlignment.self] = newValue }
+    }
+}
+
+private struct NumberTextField_TextAlignment: EnvironmentKey {
+    static let defaultValue: NSTextAlignment = .center
+}
+
+
+// MARK: - Font
+extension EnvironmentValues {
+    ///. The environment value of `NumberTextField_Font`.
+    public var numberTextField_Font: UIFont {
+        get { self[NumberTextField_Font.self] }
+        set { self[NumberTextField_Font.self] = newValue }
+    }
+}
+
+private struct NumberTextField_Font: EnvironmentKey {
+    static let defaultValue: UIFont = UIFont.preferredFont(forTextStyle: .body)
+}
+
+extension UIFont {
+    public func withWeight(_ weight: UIFont.Weight) -> UIFont {
+        var attributes = fontDescriptor.fontAttributes
+        var traits = (attributes[.traits] as? [UIFontDescriptor.TraitKey: Any]) ?? [:]
+
+        traits[.weight] = weight
+
+        attributes[.name] = nil
+        attributes[.traits] = traits
+        attributes[.family] = familyName
+        
+        let descriptor = UIFontDescriptor(fontAttributes: attributes)
+
+        return UIFont(descriptor: descriptor, size: pointSize)
+    }
+    
+    public class func preferredFont(from font: Font) -> UIFont {
+        switch font {
+        case .largeTitle:
+            return UIFont.preferredFont(forTextStyle: .largeTitle)
+        case .title:
+            return UIFont.preferredFont(forTextStyle: .title1)
+        case .title2:
+            return UIFont.preferredFont(forTextStyle: .title2)
+        case .title3:
+            return UIFont.preferredFont(forTextStyle: .title3)
+        case .headline:
+            return UIFont.preferredFont(forTextStyle: .headline)
+        case .subheadline:
+            return UIFont.preferredFont(forTextStyle: .subheadline)
+        case .callout:
+            return UIFont.preferredFont(forTextStyle: .callout)
+        case .caption:
+            return UIFont.preferredFont(forTextStyle: .caption1)
+        case .caption2:
+            return UIFont.preferredFont(forTextStyle: .caption2)
+        case .footnote:
+            return UIFont.preferredFont(forTextStyle: .footnote)
+        case .body:
+            return UIFont.preferredFont(forTextStyle: .body)
+        default:
+            return UIFont.preferredFont(forTextStyle: .body)
+        }
+    }
+}
--- Sources/NumberTextField/Components/NumberTextFieldViewRep.swift
+++ Sources/NumberTextField/Components/NumberTextFieldViewRep.swift
@@ -9,9 +9,6 @@ import SwiftUI
 import UIKit
 
 struct NumberTextFieldViewRep: UIViewRepresentable {
-    @Environment(\.multilineTextAlignment) private var textAlignment: TextAlignment
-    @Environment(\.font) private var font: Font?
-    
     var placeholder: String
     @Binding var value: Decimal?
     var formatter: NumberFormatter
@@ -73,59 +70,8 @@ extension NumberTextFieldViewRep {
      */
     private func setModifiers(_ textField: UITextField, environment: EnvironmentValues) {
         textField.keyboardType = .decimalPad
-        
-        textField.textAlignment = NSTextAlignment.getAlignment(from: self.textAlignment)
-        
-        if let font = self.font {
-            textField.font = UIFont.preferredFont(from: font)
-        }
-    }
-}
-
-
-// MARK: - NSTextAlignment
-extension NSTextAlignment {
-    static func getAlignment(from alignment: TextAlignment) -> NSTextAlignment {
-        switch alignment {
-        case .leading:
-            return .left
-        case .center:
-            return .center
-        case .trailing:
-            return .right
-        }
-    }
-}
-
-
-// MARK: - UIFont
-extension UIFont {
-    class func preferredFont(from font: Font) -> UIFont {
-        switch font {
-        case .largeTitle:
-            return UIFont.preferredFont(forTextStyle: .largeTitle)
-        case .title:
-            return UIFont.preferredFont(forTextStyle: .title1)
-        case .title2:
-            return UIFont.preferredFont(forTextStyle: .title2)
-        case .title3:
-            return UIFont.preferredFont(forTextStyle: .title3)
-        case .headline:
-            return UIFont.preferredFont(forTextStyle: .headline)
-        case .subheadline:
-            return UIFont.preferredFont(forTextStyle: .subheadline)
-        case .callout:
-            return UIFont.preferredFont(forTextStyle: .callout)
-        case .caption:
-            return UIFont.preferredFont(forTextStyle: .caption1)
-        case .caption2:
-            return UIFont.preferredFont(forTextStyle: .caption2)
-        case .footnote:
-            return UIFont.preferredFont(forTextStyle: .footnote)
-        case .body:
-            return UIFont.preferredFont(forTextStyle: .body)
-        default:
-            return UIFont.preferredFont(forTextStyle: .body)
-        }
+        textField.textAlignment = environment.numberTextField_TextAlignment
+        textField.font = environment.numberTextField_Font
+        textField.textColor = UIColor(environment.numberTextField_TextColor)
     }
 }
